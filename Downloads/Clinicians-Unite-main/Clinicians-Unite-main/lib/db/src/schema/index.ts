1d91a474049ff0e11c4cf946b51478bde1c6d37c
Mohd Ayaan Hussain <142593144+Ayaanhussain2806@users.noreply.github.com>
2026-04-29 11:32:55 +0530

    Delete Downloads/Clinicians-Unite-main/Clinicians-Unite-main directory

--- Downloads/Clinicians-Unite-main/Clinicians-Unite-main/lib/db/src/schema/index.ts
+++
@@ -1,6 +0,0 @@
-export * from "./prescriptions";
-export * from "./workflow_logs";
-export * from "./doctors";
-export * from "./insurance";
-export * from "./patients";
-export * from "./appointments";
